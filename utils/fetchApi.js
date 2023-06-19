d5e2785597b8245cdbba76029015d5f7b05e2906
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-06-19 08:58:03 -0500

    Remove unnecessary files

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const baseUrl = "https://bayut.p.rapidapi.com";
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "2ca516c5edmsh21005534119e3dep1695ddjsne4d8c281ffd6",
-      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+      'X-RapidAPI-Key': '2ca516c5edmsh21005534119e3dep1695ddjsne4d8c281ffd6',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
     },
   });
   return data;
